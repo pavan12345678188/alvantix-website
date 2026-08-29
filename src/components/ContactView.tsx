4cae5deb535fcce7e8fbb724fc1da1e100534c27
Pavansatyasai Talabattula <pavansatyasai4@gmail.com>
2026-08-29 16:41:02 +0530

    Fix contact API endpoint

--- src/components/ContactView.tsx
+++ src/components/ContactView.tsx
@@ -42,7 +42,7 @@ const handleSubmit = async (e: React.FormEvent) => {
   setIsSubmitting(true);
 
   try {
-    const response = await fetch("https://alvantix-website.onrender.com/", {
+    const response = await fetch("https://alvantix-website.onrender.com/api/contact", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
